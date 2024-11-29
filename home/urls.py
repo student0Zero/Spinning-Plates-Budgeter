1d0fd559b954d234b525a06b9538323ad6fcaaf3
Ash Hawrani <dkwqhej8@students.codeinstitute.net>
2024-11-29 16:04:17 +0000

    Basic CRUD functions in place

--- home/urls.py
+++ home/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.Index.as_view(), name = 'home'),
-    path('create_expense', views.create_expense, name = 'create_expense')
+    path('view_expenses', views.view_expenses, name = 'view_expenses'),
+    path('create_expense', views.create_expense, name = 'create_expense'),
+    path('edit_expense/<id>', views.edit_expense, name = 'edit_expense'),
+    path('delete_expense/<id>', views.delete_expense, name = 'delete_expense')
 ]
--- home/views.py
+++ home/views.py
@@ -1,12 +1,22 @@
 # using django generic class based viewa
 from django.views.generic import TemplateView
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Expense
 from .forms import ExpenseForm
 
 
 class Index(TemplateView):
     template_name = 'home/index.html'
 
+
+def view_expenses(request):
+    expenses = Expense.objects.filter(user=request.user)
+
+    context = {
+        "expenses": expenses,
+    }
+
+    return render(request, 'home/view_expenses.html', context)
 
 def create_expense(request):
     if request.method == "POST":
@@ -26,3 +36,35 @@ def create_expense(request):
             "form": form,
         }
         return render(request, 'home/create_expense.html', context)
+
+
+def edit_expense(request, id):
+    expense = get_object_or_404(Expense, id=id)
+    if request.method == "POST":
+        form = ExpenseForm(request.POST, instance=expense)
+        if form.is_valid():
+            expense = form.save(commit=False)
+            expense.user = request.user
+            expense.save()
+            # messages.success(request, "Expense edited.")
+            return redirect('home')
+        else:
+            print(form.errors)
+            return redirect('home')
+    else:
+        form = ExpenseForm(instance=expense)
+        context = {
+            "form": form,
+        }
+        return render(request, 'home/edit_expense.html', context)
+
+
+
+def delete_expense(request, id):
+    expense = get_object_or_404(Expense, id=id)
+    if request.method == "POST":
+        expense.delete()
+        # messages.success(request, "Expense deleted successfully.")
+        return redirect("home")
+    else:
+        return render(request, 'home/delete_expense.html')
