d1b63d75508897c514c91d5b3fc793f489558471
Ash Hawrani <dkwqhej8@students.codeinstitute.net>
2024-12-02 23:01:41 +0000

    Corrected view_income.html not displaying

--- income/forms.py
+++ income/forms.py
@@ -1,8 +1,9 @@
 from django import forms
-from . models import Income
+from .models import Income
 
 class IncomeForm(forms.ModelForm):
     class Meta:
+        model = Income
         fields = ['date', 'in_amount', 'description', 'category']
         widgets = {
             'date': forms.DateInput(attrs={'type': 'date'}),
