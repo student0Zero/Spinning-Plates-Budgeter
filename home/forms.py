a87164098d71a2e6c709c538399449033b5e044f
Ash Hawrani <dkwqhej8@students.codeinstitute.net>
2024-12-04 21:54:22 +0000

    Updated expense form.py to allow for negative numbers for all expense entries

--- home/forms.py
+++ home/forms.py
@@ -9,3 +9,12 @@ class ExpenseForm(forms.ModelForm):
             'date': forms.DateInput(attrs={'type': 'date'}),
             'description': forms.Textarea(attrs={'rows': 3, 'cols': 40}),  # Customize the size of the description field
         }
+    
+    # Custom clean method to ensure the amount is negative for expenses.
+    def clean_amount(self):
+        amount = self.cleaned_data.get('amount')
+        if amount > 0:
+            amount = -amount  # Convert to negative
+        elif amount == 0:
+            raise forms.ValidationError("The amount must be a negative number.")
+        return amount
