cbb19a6efb64f5da8e00a23108056dfde2cc9e56
Ash Hawrani <dkwqhej8@students.codeinstitute.net>
2024-12-04 23:01:56 +0000

    Add styling to Password Reset and Password Reset done pages

--- SpinningPlatesBudgeter/settings.py
+++ SpinningPlatesBudgeter/settings.py
@@ -18,6 +18,9 @@ import dj_database_url
 if os.path.exists('env.py'):
     import env
 
+# Email configuration for development
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
