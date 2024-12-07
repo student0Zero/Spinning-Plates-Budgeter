fc5cbddebbecd32b9acba8dcb5f8622ef798c36c
Ash Hawrani <dkwqhej8@students.codeinstitute.net>
2024-12-07 21:07:04 +0000

    Minor update of footer.html

--- static/scripts/footer.js
+++ static/scripts/footer.js
@@ -1,17 +1,19 @@
-document.addEventListener('DOMContentLoaded', (event) => {
-    // Load the saved theme
-    const themeToggler = document.getElementById('theme-toggler');
-    themeToggler.addEventListener('click', () => {
-      document.body.classList.toggle('dark-mode');
-      localStorage.setItem(
-        'theme',
-        document.body.classList.contains('dark-mode') ? 'dark' : 'light'
-      );
-    });
+
+// Dark mode toggler
+// document.addEventListener('DOMContentLoaded', (event) => {
+//     // Load the saved theme
+//     const themeToggler = document.getElementById('theme-toggler');
+//     themeToggler.addEventListener('click', () => {
+//       document.body.classList.toggle('dark-mode');
+//       localStorage.setItem(
+//         'theme',
+//         document.body.classList.contains('dark-mode') ? 'dark' : 'light'
+//       );
+//     });
   
-    // Load the saved theme
-    if (localStorage.getItem('theme') === 'dark') {
-      document.body.classList.add('dark-mode');
-    }
-  });
+//     // Load the saved theme
+//     if (localStorage.getItem('theme') === 'dark') {
+//       document.body.classList.add('dark-mode');
+//     }
+//   });
   
