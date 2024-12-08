3ec0b5a119f599457f6106c4e615925e05054d06
Ash Hawrani <dkwqhej8@students.codeinstitute.net>
2024-12-08 00:17:30 +0000

    Update all charts to use month summation of income/expense data insetad of day. Updated home/view.py/ income/view_income.html / expense/view_expense.html

--- home/views.py
+++ home/views.py
@@ -4,27 +4,27 @@ from expenses.models import Expense
 from income.models import Income
 from django.utils.dateformat import DateFormat
 from django.contrib.auth.decorators import login_required
+from django.db.models.functions import TruncMonth
 
 @login_required
 def home_view(request):
     expenses = Expense.objects.filter(user=request.user)
     incomes = Income.objects.filter(user=request.user)
 
-    # prepare data for charts.js
-    # update to use 'category' instead of 'category__expense_type' and 'category__income_type'
-    expense_summary = expenses.values('date', 'category').annotate(total_amount = Sum('amount'))
-    income_summary = incomes.values('date', 'category').annotate(total_amount = Sum('in_amount'))
+    # Aggregate by month
+    expense_summary = expenses.annotate(month=TruncMonth('date')).values('month').annotate(total_amount=Sum('amount')).order_by('month')
+    income_summary = incomes.annotate(month=TruncMonth('date')).values('month').annotate(total_amount=Sum('in_amount')).order_by('month')
 
-    # calculate the sum of all expenses and income for the logged in user
+    # Calculate the sum of all expenses and income for the logged-in user
     total_expenses = expenses.aggregate(Sum('amount'))['amount__sum'] or 0
     total_income = incomes.aggregate(Sum('in_amount'))['in_amount__sum'] or 0
 
-    # prepare data for charts.js
-    # convert data to string for home.html view
-    expense_labels = [DateFormat(item['date']).format('Y-m-d') for item in expense_summary]
+    # Prepare data for Chart.js
+    # Convert the date to a string for home.html view
+    expense_labels = [DateFormat(item['month']).format('Y-m') for item in expense_summary]
     expense_data = [float(item['total_amount']) for item in expense_summary]
-    # convert date to string for home.html view
-    income_labels = [DateFormat(item['date']).format('Y-m-d') for item in income_summary]
+    # Convert the date to a string for home.html view
+    income_labels = [DateFormat(item['month']).format('Y-m') for item in income_summary]
     income_data = [float(item['total_amount']) for item in income_summary]
 
     context = {
